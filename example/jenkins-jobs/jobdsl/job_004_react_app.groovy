f862ffc8bdaef73407801a7f78e1b47807f7daa4
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-07-20 12:22:57 -0300

    fix: copy app to container

--- example/jenkins-jobs/jobdsl/job_004_react_app.groovy
+++ example/jenkins-jobs/jobdsl/job_004_react_app.groovy
@@ -1,9 +1,22 @@
 pipelineJob('job_004_react_app') {
   description 'Build a job from jenkinsfile inside real project'
   definition {
-    cps {
-      script(readFileFromWorkspace('example/app01/Jenkinsfile'))
-      sandbox()
+    // cps {
+    //   script(readFileFromWorkspace('example/app01/Jenkinsfile'))
+    //   sandbox()
+    // }
+    // }
+    // }
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("${LOCAL_USER}@host.docker.internal:${LOCAL_JOBS}/.git")
+            credentials('ssh-local-key')
+          }
+        }
+      }
+      scriptPath('example/app01/Jenkinsfile')
     }
   }
 }
