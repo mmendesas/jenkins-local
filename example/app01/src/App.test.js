c210dcde89634a4bde025ad83afd8eca925b4de2
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-07-20 13:10:22 -0300

    feat: move react app to own repo

--- example/app01/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Dad Jokes title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Dad Jokes/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- example/jenkins-jobs/jobdsl/job_004_react_app.groovy
+++ example/jenkins-jobs/jobdsl/job_004_react_app.groovy
@@ -1,22 +1,16 @@
 pipelineJob('job_004_react_app') {
   description 'Build a job from jenkinsfile inside real project'
   definition {
-    // cps {
-    //   script(readFileFromWorkspace('example/app01/Jenkinsfile'))
-    //   sandbox()
-    // }
-    // }
-    // }
     cpsScm {
       scm {
         git {
           remote {
-            url("${LOCAL_USER}@host.docker.internal:${LOCAL_JOBS}/.git")
+            url("${LOCAL_USER}@host.docker.internal:${LOCAL_WORKSPACE}/jenkins-app-test/.git")
             credentials('ssh-local-key')
           }
         }
       }
-      scriptPath('example/app01/Jenkinsfile')
+      scriptPath('Jenkinsfile')
     }
   }
 }
